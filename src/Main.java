500ad54f5c4a04708a2860556a295c64d166e6e9
vladimir <snigereff.v@yandex.ru>
2023-01-24 01:16:34 +0300

    homework

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,16 @@
 public class Main {
     public static void main(String[] args) {
 
-        String palindrome = "ABCBA";
+        String palindrome = "ABСBA";
 
         for (int i = 0; i < palindrome.length() / 2 ; i++) {
 
-            if (palindrome.charAt(i) == palindrome.charAt(palindrome.length() - 1 -i)) {
-                System.out.println("Palindrome");
-                break;
-            }
-            else {
+            if (palindrome.charAt(i) != palindrome.charAt(palindrome.length() - 1 - i)) {
+
                 System.out.println("Not Palindrome");
-                break;
+                return;
             }
         }
+        System.out.println("Palindrome");
     }
 }
